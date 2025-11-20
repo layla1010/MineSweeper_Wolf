e2aa68d198a00b45a9d0e27bffdc5b0cae2cce09
Layla <laylahallumi@gmail.com>
2025-11-21 01:37:11 +0200

    player class

--- src/main/java/model/Player.java
+++ src/main/java/model/Player.java
@@ -1,5 +1,36 @@
 package model;
 
-public class Player {
+import java.util.Objects;
 
+public class Player {
+	
+    private String officialName;
+    private String password;
+    
+    public Player(String officialName, String password) {
+    	setOfficialName(officialName);
+    	setPassword(password);
+    }
+    
+    public String getOfficialName() {
+		return officialName;
+	}
+    
+    public void setOfficialName (String officialName) {
+    	if(officialName == null || officialName.trim().isEmpty()) {
+    		throw new IllegalArgumentException("Official Name cannot be empty");
+    	}
+    	this.officialName = officialName.trim();
+    }
+    
+    public void setPassword (String password) {
+    	if(password == null || password.trim().isEmpty()) {
+    		throw new IllegalArgumentException("password cannot be empty");
+    	}
+    	this.password = password.trim();
+    }
+
+	
+    
+	
 }
