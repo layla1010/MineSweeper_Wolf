43be9d6cd56ccbe8bee561a1510773e2e2296b5a
ayanajeeb <ayanajeeb22.3@gmail.com>
2025-11-22 13:43:56 +0200

    Create Difficulty.java

---
+++ src/main/java/model/Difficulty.java
@@ -0,0 +1,11 @@
+package model;
+
+/**
+* Difficulty level of a game board.
+* Each level controls the board size and the amount of mines, question cells and surprise cells.
+*/
+public enum Difficulty {
+ EASY,
+ MEDIUM,
+ HARD
+}
