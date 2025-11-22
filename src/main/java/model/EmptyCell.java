9079e13f2e4488584b6271af2ce02b906835797f
ayanajeeb <ayanajeeb22.3@gmail.com>
2025-11-22 16:09:20 +0200

    Update EmptyCell.java

--- src/main/java/model/EmptyCell.java
+++ src/main/java/model/EmptyCell.java
@@ -1,5 +1,17 @@
 package model;
 
-public class EmptyCell extends Cell{
+/**
+* Represents a regular empty cell.
+* The cell is not a mine, not a question and not a surprise.
+* It may still display a number (adjacent mines) in the UI.
+*/
+public class EmptyCell extends Cell {
+    public EmptyCell(int row, int col) {
+        super(row, col);
+    }
 
+    @Override
+    public CellType getType() {
+        return CellType.EMPTY;
+    }
 }
