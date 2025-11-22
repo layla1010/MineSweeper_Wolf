7da066c880c0b8b5892a220a1bcfb96bf320ea71
ayanajeeb <ayanajeeb22.3@gmail.com>
2025-11-22 13:51:06 +0200

    Update Difficulty.java

--- src/main/java/model/Difficulty.java
+++ src/main/java/model/Difficulty.java
@@ -4,7 +4,6 @@ package model;
 * Difficulty level of a game board.
 * Each level controls the board size and the amount of mines, question cells and surprise cells.
 */
-
 public enum Difficulty {
  EASY,
  MEDIUM,
