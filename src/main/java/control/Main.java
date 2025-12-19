d960678885e015d1f7ead58f5683a1956f9c5355
Layla <laylahallumi@gmail.com>
2025-12-19 15:09:06 +0200

    Update Main.java

--- src/main/java/control/Main.java
+++ src/main/java/control/Main.java
@@ -16,8 +16,8 @@ public class Main extends Application {
         SysData.getInstance().loadHistoryFromCsv();
         SysData.getInstance().loadPlayersFromCsv();
 
-       // FXMLLoader loader = new FXMLLoader(getClass().getResource("/view/players_login_view.fxml"));
-        FXMLLoader loader = new FXMLLoader(getClass().getResource("/view/history_view.fxml"));
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/view/players_login_view.fxml"));
+       // FXMLLoader loader = new FXMLLoader(getClass().getResource("/view/history_view.fxml"));
         Parent root = loader.load(); 
 
         Scene scene = new Scene(root);//, 1200, 740);
